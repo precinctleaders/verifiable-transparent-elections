f0c00eb65a5eef2e925d3ea52f19fa558f299767
precinctleaders <90533588+precinctleaders@users.noreply.github.com>
2023-01-21 17:59:49 -0500

    Update header.php

--- header.php
+++ header.php
@@ -19,12 +19,6 @@ function mrs($inp) {
     return $inp;
 } 
 
-function nicedate($stamp)
-{
-	//  convert YYYY-MM-DD to MM/DD/YY
-	return $stamp[5].$stamp[6]."/".$stamp[8].$stamp[9]."/".$stamp[2].$stamp[3];
-}
-
 function redalert($text) {
 	//create highlight box around input text
 	echo "<table width='100%' cellpadding='10px' style='border:2px solid #700;box-shadow: 2px 2px 2px #999;'><tr><td style='border-radius:2px 5px 2px 0px;background-image:linear-gradient(#d90, #ff0);color:#000;padding:10px;'><b>" . $text . "</b></td></tr></table><br><br>";
@@ -80,12 +74,82 @@ if ($_REQUEST['logout'] == 'Y')
 	<meta property="og:title" content="Washtenaw GOP Voting System">
 	<meta property="og:type" content="website">
 	<meta property="og:description" content="Welcome to Our Voting System">
+	<!-- link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css" -->
+	<style type='text/css'>
+		html, body { font-size: 16px; }
+		body {
+			font-size: 1.25rem;
+      /* font-family: Montserrat, Helvetica, Arial, sans-serif; */
+			font-family: Helvetica, Arial, sans-serif;
+		}
+		div { max-width: 90vw; }
+		h1, h2, h3, h4, h5, h6 { font-family: serif; }
+		h1 { font-size: 2rem; }
+		h2 { font-size: 1.7rem; }
+		h3 { font-size: 1.5rem; }
+		h4 { font-size: 1.3rem; }
+		h5 { font-size: 1.1rem; }
+		h6 { font-size: 1.05rem; }
+		.admin-tools {
+			width: 100vw;
+			overflow: auto;
+			background: #efe;
+			padding: 10px;
+			border: 1px solid #000;
+		}
+		table, tr, th, td {
+			font-size: 1.25rem;
+		}
+		th {
+			color: #fff;
+			background-color: #314C76;
+		}
+		th, td {
+			padding: 0.5rem;
+		}
+		.big-btn {
+			font-size: 1.75rem;
+			background-color: rgba(49, 76, 118, 0.85);
+			color: #FFF;
+			padding: 5px;
+		}
+		.candidate > .checkbox {
+			width:  1.75rem;
+			height: 1.75rem;
+			position: relative;
+			top: 0.25rem;
+			margin-right: 1.33rem;
+		}
+		.candidate > .checkbox:checked + .label {
+			color: blue;
+			font-weight: bold;
+			margin-right: 1rem;
+		}
+		.candidate > .label {
+			font-size: 1.75rem;
+		}
+		#votesremaining {
+			display: block;
+		  position: fixed;
+			right: 1.5rem;
+			bottom: 1.5rem;
+			border: 3px double #000;
+			padding: 5px;
+			background: #ff6;
+			width: 7rem;
+		}
+		#votesremainingcount {
+			font-size: 2.75rem;
+			font-weight: bold;
+			text-align: center;
+		}
+	</style>
 </head>
 <body>
 
 <?
 //Check to see if the database has been set up. If not, run the installation program and nothing else.
-if (!$admincheck = mysqli_fetch_array(mysqli_query($_SERVER['con'],"SELECT * FROM `election_users` WHERE `is_admin` = 'Y'")))
+if (!$admincheck = mysqli_fetch_array(mysqli_query($_SERVER['con'],"SELECT * FROM `election_voters` WHERE `is_admin` = 'Y'")))
 {
 	require('install.php');
 	exit;
@@ -94,7 +158,7 @@ if (!$admincheck = mysqli_fetch_array(mysqli_query($_SERVER['con'],"SELECT * FRO
 if (is_numeric($_SESSION['id']))
 {
 	//double-check admin status every time page refreshes
-	if ($self = mysqli_fetch_array(mysqli_query($_SERVER['con'],"SELECT * FROM `election_users` WHERE `id` = " . mrs($_SESSION['id']))))
+	if ($self = mysqli_fetch_array(mysqli_query($_SERVER['con'],"SELECT * FROM `election_voters` WHERE `id` = " . mrs($_SESSION['id']))))
 	{
 		$_SESSION['is_admin'] = $self['is_admin'];
 	}
