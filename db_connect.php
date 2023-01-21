cf5ce0db8f6ff4d2d72cadcd85d773fc5f3948fa
precinctleaders <90533588+precinctleaders@users.noreply.github.com>
2023-01-21 17:58:50 -0500

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,11 +1,19 @@
 <?php
 $uri = explode("/",$_SERVER['PHP_SELF']);
-if ($uri[(count($uri)-1)] != 'index.php') die('Hacking attempt.');
+if ($uri[(count($uri)-1)] != 'index.php') die('Hacking attempt.'); //this page can only be called from the one master web page and will otherwise stop
 
-$_SERVER['con'] = mysqli_connect("localhost","username","password");
+$_SERVER['con'] = mysqli_connect("host","username","password");
 if (!$_SERVER['con'])
 {
-   die('Could not connect: ' . mysqli_error($_SERVER['con']));
+   echo 'Could not connect to database.<br><br>';
+   echo 'Would you like a random new password? Try one of these:<br><br>';
+   for ($x = 0; $x<20; $x++)
+   {
+		random_str() . random_str(64,'O0o1lI') . "<br>";   
+   }
+   exit;
 }
-mysqli_select_db($_SERVER['con'],"dbname");
+mysqli_select_db($_SERVER['con'],"db85847_washtenawgop");
+
+$_SERVER['title'] = "Washtenaw County Republican Convention";
 ?>
